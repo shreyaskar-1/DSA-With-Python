5c096d7e16cd2d72bd32460754f3070246b22a8d
shreyaskar-1 <shreyaskar21@gmail.com>
2024-07-08 23:36:11 +0530

    Pyramids Pattern

---
+++ 3rd pyramid shape pattern.py
@@ -0,0 +1,16 @@
+# num = int(input("Enter The Number Of Rows: "))
+# for i in range(num):
+#     for j in range(num - i - 1):
+#         print(" ", end="")
+#     for j in range(2 * i + 1):
+#         print("*", end="")
+#     print("")
+
+
+num = int(input("Enter The Number Of Rows: "))
+for i in range(0,num):
+    for j in range(0,num - i - 1):
+        print(end=" ")
+    for j in range(0,i + 1):
+        print("*", end=" ")
+    print()
