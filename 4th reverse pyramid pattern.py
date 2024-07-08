c1b076f186f3f27c5e06ec84b31d27075d734f99
shreyaskar-1 <shreyaskar21@gmail.com>
2024-07-09 00:00:11 +0530

    changes

--- 4th reverse pyramid pattern.py
+++ 4th reverse pyramid pattern.py
@@ -1,12 +1,3 @@
-# num = int(input("Enter The Number Of Rows: "))
-# for i in range(num, 0, -1):
-#     for j in range(num - i):
-#         print(" ", end="")
-#     for j in range(i):
-#         print("* ", end="")
-#     print()
-
-
 num = int(input("Enter The Number Of Rows: "))
 for i in range(num, 0, -1):
     for j in range(num - i):
@@ -14,3 +5,12 @@ for i in range(num, 0, -1):
     for j in range(i):
         print("* ", end="")
     print()
+
+
+# num = int(input("Enter The Number Of Rows: "))
+# for i in range(num, 0, -1):
+#     for j in range(num - i):
+#         print(" ", end="")
+#     for j in range(i):
+#         print("*", end="")
+#     print()
