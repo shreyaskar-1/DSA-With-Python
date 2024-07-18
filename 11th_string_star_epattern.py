dac41b5529c2a7fbf07d9c59677fe4438a393d9c
shreyaskar-1 <shreyaskar21@gmail.com>
2024-07-18 23:54:30 +0530

    new pattern

--- 11th_string_star_epattern.py
+++ 11th_string_star_epattern.py
@@ -1,6 +1,6 @@
 for row in range (7):
     for col in range(5):
-        if (col == 0 ):
+        if (col == 0 ) or (row== 0 or row ==3 or row== 6) and (col>0):
             print ("*", end=" ")
         else:
             print (end=" ")
