646999703e099b7e9f93e6998d3b6222a47faa39
shreyaskar-1 <shreyaskar21@gmail.com>
2024-08-23 17:57:14 +0530

    new pattern

---
+++ 48th_numbers_in_RAT_shape2.py
@@ -0,0 +1,12 @@
+n = int(input("Enter The Number Of Rows: "))
+for i in range(n):
+    for j in range(i+1):
+        x = 0
+        for k in range(j):
+            x = x+n-k
+        if j % 2 == 0:
+            print(x + i - j + 1, end=" ")
+        else:
+            print(x + n - i,end = " ")
+            
+    print()
