cecfb6703e6f69af5b84dd513a4a874568a14df7
shreyaskar-1 <shreyaskar21@gmail.com>
2024-08-26 23:36:24 +0530

    new pattern

---
+++ 49th_stars_in_different_patterns.py
@@ -0,0 +1,33 @@
+# num = int(input("Enter The Number Of Rows : "))
+# for i in range(num):
+#     for j in range(num):
+#         print("$",end=" ")
+#     print()
+
+# num = int(input("Enter The Number Of Rows : "))
+# for i in range(num):
+#     print("* "*i)
+
+# num = int(input("Enter The Number Of Rows : "))
+# for i in range(1,num+1):
+#     print("*"*i)
+
+# num = int(input("Enter The Number Of Rows : "))
+# for i in range(1,num+1):
+#     print(" "*(num-i)+"*"*i)
+
+# num = int(input("Enter The Number Of Rows : "))
+# for i in range(1,num+1):
+#     print(" "*(num-i)+"* "*i)
+# for i in range(num,0,-1):
+#     print(" "*(num-i)+"* "*i)
+
+# num = int(input("Enter The Number Of Rows : "))
+# for i in range(num,0,-1):
+#     print(" "*(num-i)+"* "*i)
+
+# num = int(input("Enter The Number Of Rows : "))
+# for i in range(1,num+1):
+#     print("* "*i)
+# for i in range(num,0,-1):
+#     print("* "*i)
