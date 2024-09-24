15ca07a3bc0db8d8aab2fd65d043b3662837338b
shreyaskar-1 <shreyaskar21@gmail.com>
2024-09-25 00:00:29 +0530

    ne pattern

--- 56th_Hollow_Diamond_Shape.py
+++ 56th_Hollow_Diamond_Shape.py
@@ -1,9 +1,18 @@
 n = int(input("Enter the number of rows : "))
-for i in range (n,0,-1):
-    for j in range (i,0,-1):
-        print(j,end=" ")
-    for j in range (2*(n-i)):
+for row in range (n,0,-1):
+    for col in range (row,0,-1):
+        print("*",end=" ")
+    for col in range (2*(n-row)):
         print(" ",end=" ")
-    for j in range(i,0,-1):
+    for col in range(row,0,-1):
+        print("*",end=" ")
+    print()
+
+for row in range(n):
+    for col in range (row+1):
+        print("*",end=" ")
+    for col in range (2*(n-row-1)):
+        print(" ",end=" ")
+    for col in range(row+1):
         print("*",end=" ")
     print()
